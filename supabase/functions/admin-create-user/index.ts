d8c264b41a654dba53fe9f99b6c354827f3c13ba
Phil Armstrong <phil.armstrong@itv.com>
2023-05-26 18:01:28 +0100

    admin create user

---
+++ supabase/functions/admin-create-user/index.ts
@@ -0,0 +1,40 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+console.log("Hello from Functions!")
+
+serve(async (req) => {
+  const { email, password, payeeId } = await req.json()
+
+
+  const supabase = createClient(
+    // Supabase API URL - env var exported by default.
+    Deno.env.get('SUPABASE_URL') ?? '',
+    // Supabase API ANON KEY - env var exported by default.
+    Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+    // Create client with Auth context of the user that called the function.
+    // This way your row-level-security (RLS) policies are applied.
+    { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+  );
+
+  const { data, error } = await supabase.auth.admin.createUser({
+    email,
+    password,
+    app_metadata: { payeeId },
+  });
+
+  return new Response(
+    JSON.stringify(`Created user for ${email}`),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
